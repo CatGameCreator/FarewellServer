eb6d8a83457688725eaf2b8a7b2af480b5dad5e0
CatGameCreator <98333413+CatGameCreator@users.noreply.github.com>
2024-06-12 17:50:32 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,9 +1,13 @@
-if exist "C:\Users\ivang\OneDrive\Документы\Farewell Server" (
+if exist "C:\Users\ivang\Documents\Farewell\FarewellServer" (
   echo updating
-  cd "C:\Users\ivang\OneDrive\Документы\Farewell Server"
+  cd "C:\Users\ivang\Documents\Farewell\FarewellServer"
+  git stash
   git pull
+  git stash pop
 ) else (
   echo creating
-  cd "C:\Users\ivang\OneDrive\Документы\Farewell Server" 
-  git clone https://github.com/CatGameCreator/FarewellSMPServer.git
+  cd "C:\Users\ivang\Documents\Farewell" 
+  git clone https://github.com/CatGameCreator/FarewellServer.git
 )
+timeout 10
+call start.bat

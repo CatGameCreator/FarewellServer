d9c03f1e0bcf2730233fc487faed5ae05e6b8713
CatGameCreator <98333413+CatGameCreator@users.noreply.github.com>
2024-06-18 12:18:35 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -9,8 +9,18 @@ if exist "C:\Users\ivang\Documents\Farewell\FarewellServer" (
   echo creating
   cd "C:\Users\ivang\Documents\Farewell" 
   git clone https://github.com/CatGameCreator/FarewellServer.git
+  cd "C:\Users\ivang\Documents\Farewell\FarewellServer"
 )
-timeout 5
+timeout 2
+git diff --name-only HEAD origin/main | findstr /c:"update.bat" >nul
+if %errorlevel% equ 0 (
+    echo File was updated. Pulling changes...
+    run reserve.bat
+    exit
+) else (
+    echo File was not updated.
+)
+timeout 2
 set "targetGUID=3ff9831b-6f80-4830-8178-736cd4229e7b"
 for /f "tokens=2 delims=:" %%A in ('powercfg /getactivescheme') do (
     for /f "tokens=1" %%B in ("%%A") do set "currentGUID=%%B"
@@ -24,5 +34,3 @@ if /i "%currentGUID%"=="%targetGUID%" (
 )
 timeout 2
 run start.bat
-py main.py
-

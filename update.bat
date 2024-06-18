06be51d1e48db5a4940c1e874f54a6256ec21f91
CatGameCreator <98333413+CatGameCreator@users.noreply.github.com>
2024-06-18 12:11:11 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -10,6 +10,19 @@ if exist "C:\Users\ivang\Documents\Farewell\FarewellServer" (
   cd "C:\Users\ivang\Documents\Farewell" 
   git clone https://github.com/CatGameCreator/FarewellServer.git
 )
-timeout 10
+timeout 5
+set "targetGUID=3ff9831b-6f80-4830-8178-736cd4229e7b"
+for /f "tokens=2 delims=:" %%A in ('powercfg /getactivescheme') do (
+    for /f "tokens=1" %%B in ("%%A") do set "currentGUID=%%B"
+)
+if /i "%currentGUID%"=="%targetGUID%" (
+    echo The current power plan GUID is %targetGUID%.
+) else (
+    powercfg /import scheme.pow
+    timeout 2
+    powercfg /setactive 3ff9831b-6f80-4830-8178-736cd4229e7b
+)
+timeout 2
+run start.bat
 py main.py
-call start.bat
+
